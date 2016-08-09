cf4c93d0b37ba8c4a39fe81893fc2d30ea31e9fd
tsypuk <tsypuk.rb@gmail.com>
2016-08-09 11:55:07 +0300

    updated gitignore

---
+++ spring-boot-thymaleaf/src/main/java/ua/org/smartjava/SpringBootThymaleafApplication.java
@@ -0,0 +1,12 @@
+package ua.org.smartjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymaleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymaleafApplication.class, args);
+	}
+}
---
+++ spring-boot-thymaleaf/src/test/java/ua/org/smartjava/SpringBootThymaleafApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.org.smartjava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootThymaleafApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
